f092bbf1cda6b47092accca010c6a34224976b1c
marceloarauj <42809145+marceloarauj@users.noreply.github.com>
2019-07-19 22:16:41 -0300

    Feature-01

--- ModeloAspCoreMVC/Compartilhado/BancoUtil.cs
+++ ModeloAspCoreMVC/Compartilhado/BancoUtil.cs
@@ -8,9 +8,34 @@ namespace ModeloAspCoreMVC.Compartilhado
 {
     public class BancoUtil
     {
-        public static void executarQuery(string query)
+        public static Dictionary<string,List<string>> executarQuery(string query)
         {
-            NpgsqlConnection consulta = new NpgsqlConnection();
+            NpgsqlConnection consulta = 
+                new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres Password=1234;Database=github");
+
+            consulta.Open();
+
+            NpgsqlCommand comando = new NpgsqlCommand(query);
+
+            NpgsqlDataReader ler = comando.ExecuteReader();
+
+            Dictionary<string, List<string>> resultado = new Dictionary<string, List<string>>();
+
+            for(int i =0; i < ler.FieldCount;i++)
+            {
+                resultado.Add(ler.GetName(i), new List<string>());
+            }
+
+            while (ler.Read())
+            {
+                for(int i =0; i < ler.FieldCount; i++)
+                {
+                    resultado[ler.GetName(i)].Add(ler.GetValue(i).ToString());
+                }
+            }
+            consulta.Close();
+
+            return resultado;
         }
         public static void executarInsert(string insert)
         {
